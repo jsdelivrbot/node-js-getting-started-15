d6a08cf00eea8497e2817c1abe61b68691691f2f
mpramodkumar <PramodKumar.Medimuni@cognizant.com>
2017-09-19 12:20:10 +0530

    added post method

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.post('/webhook', function(request, response) {
+    console.log('webhook');
+	response.status(200).send('Trade Finance Client')
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
