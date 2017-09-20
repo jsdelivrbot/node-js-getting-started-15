e20f345585b2ea60a70a3170d2bb87897fac2e7a
mpramodkumar <PramodKumar.Medimuni@cognizant.com>
2017-09-20 17:52:56 +0530

    addded login service

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var axios = require('axios')
+var Slack = require('slack-node');
+var WebClient = require('@slack/client').WebClient;
+var tracker = require('pivotaltracker');
+var client = new tracker.Client('mytoken');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,6 +22,31 @@ app.post('/webhook', function(request, response) {
     console.log('webhook');
 	response.status(200).send('Trade Finance Client')
 });
+
+app.post('/login', function(request, response) {
+	console.log(request.query.username);
+	getGithupApiUsers(request.query.username,response);
+});
+
+function getGithupApiUsers(username,response){
+      console.log(username);
+      axios({
+	      method: 'GET',
+	      url: 'https://api.github.com/orgs/CognizantStudio/memberships/'+username,
+	      headers: {
+		Authorization: 'Basic bXByYW1vZGt1bWFyOnZpY3RvcnkyOQ==',
+		'Content-Type': 'application/json',
+	      },
+    })
+	  .then(function(res){
+		console.log('success'+res);
+		response.status(200).send(true)
+	})
+	.catch(function (error) {
+    console.log('fail'+error);
+    response.status(200).send(false)
+	});
+} 
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
