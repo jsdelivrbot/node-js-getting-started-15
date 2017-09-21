fa65f9de25a99583f9d3cf9b552bcb5e7fceb29e
mpramodkumar <PramodKumar.Medimuni@cognizant.com>
2017-09-21 19:45:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', function(request, response) {
 });
 
 app.post('/webhook', function(request, response) {
-	console.log('webhook'+request.body.parser);
+	console.log('webhook'+request.body.payload);
 	let webhookUri = "https://hooks.slack.com/services/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";
 	let slack = new Slack();
 	slack.setWebhook(webhookUri);
